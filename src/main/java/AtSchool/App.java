b3354426c146253939b701c0f4fce8a3c927a971
Dmitrii Prikhozhaev <prigho-dima@ya.ru>
2020-06-25 11:08:04 +0400

    commit #2

--- src/main/java/AtSchool/App.java
+++ src/main/java/AtSchool/App.java
@@ -4,10 +4,7 @@ package AtSchool;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args){
     }
 }
--- src/test/java/AtSchool/Kotik.java
+++ src/test/java/AtSchool/Kotik.java
@@ -9,7 +9,8 @@ public class Kotik {
         kotik.prettiness = 9999;
         kotik.name = "Рыжый";
         kotik.weight = 6327;//в граммах
-
+        kotik.meow = "Кря кря";
+        kotik.liveAnotherDay();
     }
 
     @Test
@@ -37,6 +38,8 @@ public class Kotik {
 
     int prettiness;
     int weight;
+    int satiety = 10;
+    String foodName = "Cat's food";
     String name;
     String meow;
 
@@ -47,16 +50,20 @@ public class Kotik {
     public Kotik() {
     }
 
-    public Kotik(int prettiness, int weight, String name, String meow) {this.prettiness = prettiness;
-        this.weight = weight;this.name = name;this.meow = meow;}
+//    public Kotik(int prettiness, int weight, String name, String meow) {
+//        this.prettiness = prettiness;
+//        this.weight = weight;
+//        this.name = name;
+//        this.meow = meow;
+//    }
 
 
-    void setKotik(int prettiness, String name, int weight, String meow) {
-        this.prettiness = prettiness;
-        this.weight = weight;
-        this.name = name;
-        this.meow = meow;
-    }
+//    void setKotik(int prettiness, String name, int weight, String meow) {
+//        this.prettiness = prettiness;
+//        this.weight = weight;
+//        this.name = name;
+//        this.meow = meow;
+//    }
 
     int giveMeMoney() {
         return 100;
@@ -64,5 +71,73 @@ public class Kotik {
 
     void sayMeow() {
         System.out.println(name + " говорит " + meow);
+    }
+
+    public int play(int satiety) {
+        System.out.println(name + " играет с хвостом");
+        return satiety--;
+    }
+
+    public int sleep(int satiety) {
+        System.out.println(name + " спит");
+        return satiety--;
+    }
+
+    public int chaseMouse(int satiety) {
+        System.out.println(name + " ловит мышку");
+        return satiety--;
+    }
+
+    void eat() {
+        System.out.println(name + " кушает");
+        Kotik.eat(satiety, foodName);
+    }
+
+    public static int eat(int satiety) {
+        return satiety++;
+    }
+
+    public static int eat(int satiety, String foodName) {
+        System.out.println( "Котик кушает " + foodName);
+        return satiety++;
+    }
+
+    public int walk(int satiety) {
+        System.out.println(name + " гуляет");
+        return satiety--;
+    }
+
+    public int sharpenClaws(int satiety) {
+        System.out.println(name + " точит ногти");
+        return satiety--;
+    }
+
+    void liveAnotherDay() {
+        int hour = 24;
+        while (hour >= 24) {
+            if (satiety <= 0) {
+                System.out.println("Kotik want eat");
+                Kotik.eat(satiety, foodName);
+            } else {
+                int act = (int) (Math.random() * 7 + 1);
+                switch (act) {
+                    case 1:
+                        play(satiety);
+                    case 2:
+                        sayMeow();
+                    case 3:
+                        sleep(satiety);
+                    case 4:
+                        chaseMouse(satiety);
+                    case 5:
+                        eat();
+                    case 6:
+                        walk(satiety);
+                    case 7:
+                        sharpenClaws(satiety);
+                }
+                hour--;
+            }
+        }
     }
 }
